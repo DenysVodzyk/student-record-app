0007af2cda10f6e1c594f78885f8db24823f0c2a
DenysVodzyk <vodzyk@gmail.com>
2021-11-17 12:27:36 -0500

    first commit

---
+++ src/main/java/com/example/studentrecordapp/StudentRecordAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentrecordapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRecordAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRecordAppApplication.class, args);
+	}
+
+}
